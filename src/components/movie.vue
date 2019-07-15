d31584c72109d233f2474cb39e7e91a9aac06724
Amar Pendhari <amar.pendhari97@gmail.com>
2019-07-16 00:45:57 +0530

    push

---
+++ src/components/movie.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      New Movies
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'movies',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+body{
+    margin: 0 auto;
+}
+.navbar {
+ background-color: saddlebrown;
+ padding: 10px 10px 10px 20px; 
+ font-size: 2em;
+ 
+}
+</style>
